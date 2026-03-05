7f0081ec58acf5bc18316f28e30cc81de7ab72f5
garvbarthwal <garvbarthwal@gmail.com>
2026-03-05 16:00:51 +0530

    checkpoint: url are coming but nextApp fails

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,7 +1,8 @@
 import { Agent, openai, anthropic,  createAgent } from "@inngest/agent-kit";
-
+import {Sandbox } from "@e2b/code-interpreter"
 import { inngest } from "./client";
-import { success } from "zod";
+import { step } from "inngest";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" ,
@@ -9,6 +10,10 @@ export const helloWorld = inngest.createFunction(
   },
   { event: "test/hello.world" },
   async ({ event}) => {
+    const sandboxId= await step.run("get-sandbox-id",async() =>{
+      const sandbox=await  Sandbox.create("garvbarthwal/agent-nextjs-garvbarthwal");
+      return sandbox.sandboxId;
+    })
 
      const codeagent = createAgent({
       name: "code-agent",
@@ -16,6 +21,12 @@ export const helloWorld = inngest.createFunction(
       model: openai({ model: "gpt-4.1-mini" },),
     });
     const { output } = await codeagent.run(`Write the following snippet: ${event.data.value}`);
-    return {output };
+    const sandboxUrl=await step.run("get-sandbox-url",async()=> {
+      const sandbox=await getSandbox(sandboxId);
+      const host=  sandbox.getHost(3000);
+      return `https://${host}`
+    })
+    return {output,sandboxUrl };
+
   },
 );
